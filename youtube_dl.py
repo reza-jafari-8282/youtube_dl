4668727f0a90d07889b1cb40d885783852bd4bd7
Reza Jafari <97598188+reza-jafari-8282@users.noreply.github.com>
2022-03-31 15:02:00 +0430

    Update youtube_dl.py

--- youtube_dl.py
+++ youtube_dl.py
@@ -1,23 +1,21 @@
 from pytube import YouTube
 
+class Youtube_dl:
+	def __init__(self,url):
+		self.url = url
+
+	def video(self):
+		print("downloading...")
+		YouTube(url).streams.filter(res="720p").first().download()
+
+	def audio(self):
+		print("downloading...")
+		YouTube(url).streams.filter(only_audio=True).first().download()
 
 url = input("Enter the url : ")
 media_type = input("video(1) or audio(2) : ")
 
-
-def video(url):
-	resolution = input("Enter the quality you want : ")
-	resolution = resolution + "p"
-	print("downloading...")
-	YouTube(url).streams.filter(res=resolution).first().download()
-
-
-def audio(url):
-	print("downloading...")
-	YouTube(url).streams.filter(only_audio=True).first().download()
-
-
 if media_type == "1":
-	video(url)
+	Youtube_dl.video(url)
 elif media_type == "2":
-	audio(url)
+	Youtube_dl.audio(url)
